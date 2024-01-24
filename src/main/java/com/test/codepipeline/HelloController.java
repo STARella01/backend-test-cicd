12ac344ede9ad47b88f3603a28fe54dfbbbe31e6
STARella01 <seongee2rum@gmail.com>
2024-01-24 10:10:15 +0900

    수정 배포

--- src/main/java/com/test/codepipeline/HelloController.java
+++ src/main/java/com/test/codepipeline/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @GetMapping("/")
         public String root() {
-            return "Hi This Page Before Moddifiyed";
+            return "Hi This Page After Moddifiyed- 수정 후 입니다.";
         }
 }
