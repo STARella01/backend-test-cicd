75a1a736eb772e56a7239bb472b55866730e4d5d
STARella01 <seongee2rum@gmail.com>
2024-01-24 20:03:41 +0900

    젠킨스 설정 확인 위해 수정 push3

--- src/main/java/com/test/codepipeline/HelloController.java
+++ src/main/java/com/test/codepipeline/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @GetMapping("/")
         public String root() {
-            return "Hi This Page After Moddifiyed 뭘까요???????";
+            return "Hi This Page After Moddifiyed 뭘까요???????!!!!!";
         }
 }
